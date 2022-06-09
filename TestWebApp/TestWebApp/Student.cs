43742dabe87a82d4dda055aa88742338635cc7a2
wasabi98 <61020476+wasabi98@users.noreply.github.com>
2022-06-09 16:45:30 +0200

    uploading files 2

---
+++ TestWebApp/TestWebApp/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesztWebalkalmazás
+{
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Phone { get; set; }
+        public List<Grade> Grades { get; set; }
+       
+    }
+}
